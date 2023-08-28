91d35dfc3b716546e7bb8266db531f728bff82dc
Vansham-1406 <vanshamaggarwal6@gmail.com>
2023-08-29 00:29:46 +0530

    Change

--- frontend/src/components/home/TopTags.jsx
+++ frontend/src/components/home/TopTags.jsx
@@ -16,33 +16,117 @@ const TopTags = () => {
                     </div>
                     <div className='d-flex mb-0 mt-3 singleTopBox'>
                         <div className='boxSingleTag d-flex'>
-                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>Redux</p>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>web dev</p>
                         </div>
-                            <p className='mb-0 ms-2 num'>x 4</p>
+                            <p className='mb-0 ms-2 num'>x 3</p>
                     </div>
                     <div className='d-flex mb-0 mt-3 singleTopBox'>
                         <div className='boxSingleTag d-flex'>
-                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>Redux</p>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>javascript</p>
                         </div>
-                            <p className='mb-0 ms-2 num'>x 4</p>
+                            <p className='mb-0 ms-2 num'>x 2</p>
                     </div>
                     <div className='d-flex mb-0 mt-3 singleTopBox'>
                         <div className='boxSingleTag d-flex'>
-                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>Redux</p>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>React js</p>
                         </div>
-                            <p className='mb-0 ms-2 num'>x 4</p>
+                            <p className='mb-0 ms-2 num'>x 1</p>
                     </div>
                     <div className='d-flex mb-0 mt-3 singleTopBox'>
                         <div className='boxSingleTag d-flex'>
-                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>Redux</p>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>JS</p>
                         </div>
-                            <p className='mb-0 ms-2 num'>x 4</p>
+                            <p className='mb-0 ms-2 num'>x 1</p>
                     </div>
                     <div className='d-flex mb-0 mt-3 singleTopBox'>
                         <div className='boxSingleTag d-flex'>
-                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>Redux</p>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>svelte</p>
                         </div>
-                            <p className='mb-0 ms-2 num'>x 4</p>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>svelte</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>sql</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>nosql</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>mongo db</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>postgre sql</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>mysql</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>c-sharp</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>java</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>github</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>cra</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>life</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>app</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>asp</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
+                    </div>
+                    <div className='d-flex mb-0 mt-3 singleTopBox'>
+                        <div className='boxSingleTag d-flex'>
+                            <p className='ps-2 pe-2 pt-1 mb-0 pb-1'>django</p>
+                        </div>
+                            <p className='mb-0 ms-2 num'>x 1</p>
                     </div>
                 </div>
             </div>
